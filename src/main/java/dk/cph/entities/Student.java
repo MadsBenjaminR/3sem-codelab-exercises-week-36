75b0779965b30529d00e6c82aae5ef48b047e284
Jeppe Koch <cph-jk469@cphbusiness.dk>
2024-09-04 10:02:55 +0200

    start of entities

--- src/main/java/dk/cph/entities/Student.java
+++ src/main/java/dk/cph/entities/Student.java
@@ -1,9 +1,23 @@
 package dk.cph.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
 /**
  * Purpose:
  *
  * @author: Jeppe Koch
  */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Builder
+@Table(name = "student")
 public class Student {
+
+
 }
